cba8ebfcae51d2fb92ebb96237eecdc0546681e1
lunaroyster <oneinaquadrillion@live.com>
2018-08-21 12:18:39 +0000

    Freed CORS

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,21 @@
 const functions = require('firebase-functions');
 const getSheet = require("./getSheet");
+const cors = require('cors')({origin: true});
 
 exports.getResourcesSheet = functions.https.onRequest(async (req, res) => {
+    cors(req, res, () => {});
+    if(req.method=='OPTIONS') return;
     res.send(await getSheet.resources());
 });
 
 exports.getReliefCampsSheet = functions.https.onRequest(async (req, res) => {
+    cors(req, res, () => {});
+    if(req.method=='OPTIONS') return;
     res.send(await getSheet.reliefCamps());
 });
 
 exports.getVolunteersSheet = functions.https.onRequest(async (req, res) => {
+    cors(req, res, () => {});
+    if(req.method=='OPTIONS') return;
     res.send(await getSheet.volunteers());
 });
