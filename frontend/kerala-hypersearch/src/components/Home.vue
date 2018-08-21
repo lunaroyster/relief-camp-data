0c706ea337c8f2cda9f477d5ef303114661e4243
lunaroyster <oneinaquadrillion@live.com>
2018-08-21 21:08:51 +0000

    Resource cards

--- frontend/kerala-hypersearch/src/components/Home.vue
+++ frontend/kerala-hypersearch/src/components/Home.vue
@@ -1,29 +1,57 @@
 <template>
   <div>
     <Menu/>
+    <div class="container">
+      <ResourceCard v-for="result in results.slice(0,25)" :resource="result.item"/>
+    </div>
   </div>
 </template>
 
 <script>
 import Menu from '@/components/Menu';
+import ResourceCard from '@/components/ResourceCard';
+
 import dataService from '@/services/data';
 import Fuse from 'fuse.js';
 
-console.log(dataService);
-console.log(Fuse);
+let fuse;
 
 export default {
   name: 'Home',
   components: {
     Menu,
+    ResourceCard,
   },
   data() {
-    return {};
+    return {
+      results: [],
+    };
+  },
+  computed: {
+    searchTerm: function() {
+      return this.$store.state.searchTerm;
+    },
   },
   async mounted() {
-    let volunteers = await dataService.getVolunteers();
-    let fuse = new Fuse(volunteers, {});
-    
+    let resources = await dataService.getResources();
+    let fuseConfig = {
+      shouldSort: true,
+      includeScore: true,
+      threshold: 0.6,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: [
+        "resourcesAvailableWithThem"
+      ]
+    }
+    fuse = new Fuse(resources, fuseConfig);
+  },
+  watch: {
+    searchTerm(term, exTerm) {
+      this.results = fuse.search(term);
+    }
   }
 };
 
---
+++ frontend/kerala-hypersearch/src/components/ResourceCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="resource">
+    <div class="text-muted pb-2 d-flex">
+      {{resource.resourcesAvailableWithThem}}
+      <i class="material-icons px-2" v-if="resource.verifiedOrNot=='verified'">verified_user</i>
+    </div>
+    <div class="d-flex p-2">
+      <i class="material-icons px-2">account_circle</i>
+      {{resource.nameOfPeople}}
+      {{resource.contactDetails}}
+    </div>
+    <div class="d-flex p-2" v-if="resource['location(rough)']">
+      <i class="material-icons px-2">location_on</i>
+      {{resource['location(rough)']}}
+    </div>
+    <div class="d-flex p-2" v-if="resource.comments">
+      <i class="material-icons px-2">comment</i>
+      {{resource.comments}}
+    </div>
+    <div class="d-flex p-2" v-if="resource.availability">
+      <i class="material-icons px-2">info</i>
+      {{resource.availability}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Resource',
+  data() {
+    return {};
+  },
+  props: {
+    resource: Object,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.resource {
+  margin-top: 1em;
+  padding: 1em;
+  box-sizing: border-box;
+  transition: 0.2s;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+}
+</style>
